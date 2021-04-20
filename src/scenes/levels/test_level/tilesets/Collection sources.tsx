01b7456df047c982e5744090202d22a6d2669b8f
David Queen <dsqjr@msn.com>
2021-04-19 21:30:07 -0400

    removed unused assets

--- src/scenes/levels/test_level/tilesets/Collection sources.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Collection sources" tilewidth="50" tileheight="50" tilecount="81" columns="9">
- <image source="futuristic mech pixel art assets/futuristic  mech pixel art assets/coloured decorations/foreground decoration assets.png" width="496" height="496"/>
-</tileset>
