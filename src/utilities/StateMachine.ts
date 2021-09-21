335302571157be432ab612c402d2e0270a195e34
David Queen <dsqjr@msn.com>
2021-09-21 00:08:44 -0400

    stateMachine update

--- src/utilities/StateMachine.ts
+++ src/utilities/StateMachine.ts
@@ -1,73 +1,76 @@
-interface iEvent{
-    name : string,
-    from : string | Array<string>
-    to : string | Array<string>
-    conditional? : Function
+interface iState{
+    entity : Object;
+    states : iHashmap<iState>;
+    handleInput(entity : Object, input :string) :void
+    enter() : void
+    exit() : void
+}
+interface iHashmap<Type>{
+    [name : string] : Type
 }
 
-
-interface stateSettings {
-    initial:string,
-    events : Array<iEvent>
+class State implements iState{
+    states : iHashmap<iState>;
+    handleInput : (entity : Object, i:string)=>iState;
+    enter : ()=>void;
+    exit :  ()=>void;
+    entity:any;
+    constructor(enitiy:any,  states : iHashmap<iState>, handleInput : (entity : Object, i:string)=>iState, enter = ()=>{ console.log('entering state')}, exit = ()=>{ console.log('leaving state')} ){
+        this.handleInput = handleInput;
+        this.enter = enter;
+        this.exit = exit;
+    }
 }
-
-
-
-class event{
-    name:string;
-    condition : Function;
-    to: string;
-    from: string
-    constructor(options : any){
-        this.name = options.name;
-        this.to = options.to,
-        this.from = options.from;
-    }
-    when(expression : Function): event{
-        this.condition = expression;
+let jump  = function(i:string){return this});
+let run  = function(entity : Object, i:string){
+    if(i == "left"){
+        console.log('show left sprite');
+        return this;
+    }else if( i === 'right' ){
+        console.log('show right sprite')
         return this;
     }
-    on(event:string): event{
-        //todo check if event is in list
+    else if(!i){
+        idle.enter();
+        return idle;
+    }
+};
+let idle :iState = new State({speed : 0}, function (i:string){
+ if(!i){
+    return this;
+ }else{
+     this.exit();
+     if(i === 'left'){
+         run.enter();
+         return run;
+     }
+     if(i === 'up'){
+        jump.enter();
+        return jump
+     }
+ }
+}, ()=>{console.log('enter idle state')},
+()=>{console.log('exit idle state')})
 
-        return this;
+
+class FSM{
+    enitiy :any;
+    state : iState
+    allStates : iHashmap<iState>
+    constructor(entity : any){
+        this.enitiy = entity;
+    }
+
+    run(input : string){
+        this.state = this.state.handleInput(this.enitiy, input);
         
     }
-
-}
-
-export default class StateMachine{
-
-    events : Array<event>
-    currentState : string;
-    constructor(settings: stateSettings){
-        this.currentState = settings.initial;
-        this.events = settings.events.map(e =>{
-            return new event(e);
-        });
-    }
-   
-    private transition(to : string ){
-
-        (to as unknown as event) = this.getEvent(to);
-        if(!to){
-            throw new Error('to event not found');
-        }
-        //check the conditional
-        if((to as unknown as event).condition()){
-        //check that this state can move to/from current state
-            if(!(to as unknown as event).from.includes(this.currentState)){
-                //cant move to this state
-                return;
-            }else{
-                //dispatch event
-            }
-        }
-    }
-    public getEvent(name:string):event | null{
-        return this.events.find(e => event.name === name)
+    addState(name :string, handleInput : (i:string)=>iState,  enter? : ()=>void, exit? :  ()=>void){
+        const state = new State(this.enitiy, this.allStates , handleInput,enter, exit);
+        this.allStates[name] = state;
     }
 }
-let temp = 50;
-const test = new StateMachine({initial:'liquid', events : [{name:'freeze',to:'ice',from:'liquid'}, {name:'boil',to:'steam',from:'liquid'}]});
-test.getEvent('freeze').when((temp : number)=>temp < 0)
+
+let test = new FSM({speed : 0, jumps : 0});
+
+test.addState("run",run);
