f3402b6dee61f701cea3ce2bf3fce388dd938c7d
David Queen <dsqjr@msn.com>
2021-11-16 20:47:27 -0500

    added jest

---
+++ __tests__/player.spec.js
@@ -0,0 +1,11 @@
+//check if canvas is added to page
+/** global it jest test */
+import Player from '../src/utilities/Player';
+
+describe('basic start up',()=>{
+    it('adds a canvas to the document',()=>{
+
+         document.querySelector('canvas')
+    });
+   
+});
